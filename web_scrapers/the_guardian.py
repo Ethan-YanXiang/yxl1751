f8c1150274bed216cfac4df8de1db1e77627004b
Yen-Hsiang Lin <yxl1751@student.bham.ac.uk>
2024-07-18 21:17:04 +0100

    def build_full_urls()

--- web_scrapers/the_guardian.py
+++ web_scrapers/the_guardian.py
@@ -1,34 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://www.theguardian.com/uk').text
-soup = BeautifulSoup(source, 'lxml')
 
-articles = soup.find_all('div', class_='dcr-4z6ajs')
-for article in articles:
-    # print(article.prettify())
+def build_full_urls():
+    source = requests.get('https://www.theguardian.com/uk').text
+    soup = BeautifulSoup(source, 'lxml')
 
-    try:
-        headline = article.find('span', class_='show-underline dcr-1ch1h6j').text
-        print(headline)
-    except AttributeError:
-        headline = None
+    '''construct full urls from all news page'''
+    full_urls = []
 
-    news_link = f'https://www.theguardian.com{article.a['href']}'
-    print(news_link)
+    articles = soup.find_all('div', class_='dcr-4z6ajs')
+    for article in articles:
+        # print(article.prettify())
 
-    print()
+        # try:
+        #     headline = article.find('span', class_='show-underline dcr-1ch1h6j').text
+        # except AttributeError:
+        #     headline = None
+        # full_urls.append(headline)
 
-    sub_articles = article.find_all('li', class_='dcr-8x9syc')
-    if sub_articles:
-        for sub_article in sub_articles:
-            try:
-                sub_headline = sub_article.find('span', class_='show-underline dcr-1ch1h6j').text
-                print(sub_headline)
-            except AttributeError:
-                sub_headline = None
+        full_url = f'https://www.theguardian.com{article.a['href']}'
+        full_urls.append(full_url)
 
-            sub_news_link = f'https://www.theguardian.com{sub_article.a['href']}'
-            print(sub_news_link)
+        sub_articles = article.find_all('li', class_='dcr-8x9syc')
+        if sub_articles:
+            for sub_article in sub_articles:
 
-            print()
+                sub_full_url = f'https://www.theguardian.com{sub_article.a['href']}'
+                full_urls.append(sub_full_url)
+
+    return full_urls
+
+
+# print(build_full_urls())
