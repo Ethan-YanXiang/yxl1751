296cecf6118a0b42350f669bf03a3fdd50c9a3ae
Yen-Hsiang Lin <yxl1751@student.bham.ac.uk>
2024-07-18 17:33:31 +0100

    scraping sub_articles

--- web_scrapers/the_guardian.py
+++ web_scrapers/the_guardian.py
@@ -4,7 +4,8 @@ import requests
 source = requests.get('https://www.theguardian.com/uk').text
 soup = BeautifulSoup(source, 'lxml')
 
-for article in soup.find_all('div', class_='dcr-4z6ajs'):
+articles = soup.find_all('div', class_='dcr-4z6ajs')
+for article in articles:
     # print(article.prettify())
 
     try:
@@ -17,3 +18,17 @@ for article in soup.find_all('div', class_='dcr-4z6ajs'):
     print(news_link)
 
     print()
+
+    sub_articles = article.find_all('li', class_='dcr-8x9syc')
+    if sub_articles:
+        for sub_article in sub_articles:
+            try:
+                sub_headline = sub_article.find('span', class_='show-underline dcr-1ch1h6j').text
+                print(sub_headline)
+            except AttributeError:
+                sub_headline = None
+
+            sub_news_link = f'https://www.theguardian.com{sub_article.a['href']}'
+            print(sub_news_link)
+
+            print()
