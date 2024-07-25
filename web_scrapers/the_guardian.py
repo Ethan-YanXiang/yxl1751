3bd79c59fc752ef766596ed5ff4b7c1ccd75167a
Yen-Hsiang Lin <yxl1751@student.bham.ac.uk>
2024-07-25 15:47:35 +0100

    add fake_useragent

--- web_scrapers/the_guardian.py
+++ web_scrapers/the_guardian.py
@@ -1,18 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import random
+import time
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+total_fetched_articles = 0
 
 
 def fetch_article_data(article_url):
 
-    response = requests.get(article_url).text
+    global total_fetched_articles
+
+    headers = {'User-Agent': ua.random}
+    response = requests.get(article_url, headers=headers).text
     soup = BeautifulSoup(response, 'lxml')
 
     try:
-        body = soup.find('div', class_='dcr-1qg0p6f').get_text(separator='', strip=True)
+        maincontent = soup.find('div', id='maincontent')
+        paragraphs = maincontent.find_all('p')
+        body = ' '.join(p.text for p in paragraphs)
         if len(body) == 0:
             return False
     except AttributeError:
         return False
+    time.sleep(random.uniform(1, 2))
 
     try:
         headline = soup.h1.text.strip()
@@ -27,34 +39,41 @@ def fetch_article_data(article_url):
         except AttributeError:
             date = None
 
+    total_fetched_articles += 1
+
     return headline, date, body, article_url
 
 
 def build_full_urls():
-    response = requests.get('https://www.theguardian.com/uk').text
+
+    headers = {'User-Agent': ua.random}
+    response = requests.get('https://www.theguardian.com/uk', headers=headers).text
     soup = BeautifulSoup(response, 'lxml')
 
     main_articles = soup.find_all('div', class_='dcr-4z6ajs')
     for main_article in main_articles:
         main_article_url = f'https://www.theguardian.com{main_article.a['href']}'
-        fetch_article_data(main_article_url)
-        # print_article_data(main_article_url)
+        # fetch_article_data(main_article_url)
+        print_article_data(main_article_url)
 
         sub_articles = main_article.find_all('li', class_='dcr-8x9syc')
         if sub_articles:
             for sub_article in sub_articles:
                 sub_article_url = f'https://www.theguardian.com{sub_article.a['href']}'
-                fetch_article_data(sub_article_url)
-                # print_article_data(sub_article_url)
+                # fetch_article_data(sub_article_url)
+                print_article_data(sub_article_url)
 
 
-# def print_article_data(article_url):
-#     if fetch_article_data(article_url):
-#         headline, date, body, article_url = fetch_article_data(article_url)
-#         print(f"Headline: {headline}")
-#         print(f"Date: {date}")
-#         print(f"Body: {body}")
-#         print(f"URL: {article_url}\n")
+def print_article_data(article_url):
+
+    article_data = fetch_article_data(article_url)
+    if article_data:
+        headline, date, body, article_url = article_data
+        print(f"Headline: {headline}")
+        print(f"Date: {date}")
+        print(f"Body: {body}")
+        print(f"URL: {article_url}\n")
 
 
 build_full_urls()
+print(f"Total fetched articles: {total_fetched_articles}")
